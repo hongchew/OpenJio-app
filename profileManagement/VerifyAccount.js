63a82bb5fa715b523137389e579e1fb15f5d170b
hongchew <hongchew96+github@gmail.com>
2020-10-03 01:31:43 +0800

    verify user singpass styling

--- profileManagement/VerifyAccount.js
+++ profileManagement/VerifyAccount.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {StatusBar, TouchableWithoutFeedback, Keyboard, StyleSheet} from 'react-native';
+import {
+  StatusBar,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView,
+} from 'react-native';
 import {Text, Icon, Layout, Input, Button} from '@ui-kitten/components';
 import {connect} from 'react-redux';
 import loginStyle from '../styles/loginStyle';
 import axios from 'axios';
 import {globalVariable} from '../GLOBAL_VARIABLE';
-import {editProfile} from '../redux/actions';
-
 
 class VerifyAccount extends React.Component {
   constructor(props) {
@@ -21,12 +26,26 @@ class VerifyAccount extends React.Component {
     };
   }
 
+  toggleSecureEntry = () => {
+    if (this.state.secureTextEntry) {
+      this.setState({
+        secureTextEntry: false,
+      });
+    } else {
+      this.setState({
+        secureTextEntry: true,
+      });
+    }
+  };
 
-  handleEditProfile = () => {
-    if (
-      this.state.name == '' ||
-      this.state.email == ''
-    ) {
+  renderIcon = (props) => (
+    <TouchableWithoutFeedback onPress={this.toggleSecureEntry}>
+      <Icon name={this.state.secureTextEntry ? 'eye-off' : 'eye'} {...props} />
+    </TouchableWithoutFeedback>
+  );
+
+  handleVerify = () => {
+    if (this.state.name == '' || this.state.email == '') {
       this.setState({
         isUpdated: false,
         message: 'Name and email fields cannot be empty.',
@@ -36,8 +55,8 @@ class VerifyAccount extends React.Component {
         userId: this.props.user.userId,
         name: this.state.name,
         mobileNumber: this.state.mobileNumber,
-        email: this.state.email
-      }
+        email: this.state.email,
+      };
       this.props.editProfile(user);
       setTimeout(() => {
         if (this.props.isUpdated) {
@@ -49,58 +68,67 @@ class VerifyAccount extends React.Component {
         }
       }, 500);
     }
-
   };
-
 
   render() {
     let responseMessage;
     if (this.state.isUpdated) {
       responseMessage = (
-        <Text style={loginStyle.message} status="success">
+        <Text style={loginStyle.message} status='success'>
           {this.state.message}
         </Text>
       );
     } else {
       responseMessage = (
-        <Text style={loginStyle.message} status="danger">
+        <Text style={loginStyle.message} status='danger'>
           {this.state.message}
         </Text>
       );
     }
 
     return (
-      <Layout style={styles.layout}>
-        <StatusBar
-          barStyle="dark-content"
-          hidden={false}
-          backgroundColor="#ffffff"
-          translucent={true}
-        />
-        <Text style={styles.header} category="h4">
-          Verify Account
-        </Text>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <Layout style={styles.container}>
-            <Input
-              label="NRIC"
-              value={this.state.nric}
-              onChangeText={(text) => this.setState({nric: text})}
-            />
-            <Input
-              label="Password"
-              value={this.state.password}
-              onChangeText={(text) => this.setState({password: text})}
-            />
-            
-            <Button
-              style={styles.button}
-              onPress={() => this.handleEditProfile()}>
-              UPDATE PROFILE
-            </Button>
-            {responseMessage}
+      <Layout style={loginStyle.layout}>
+        <Layout style={styles.container}>
+          <StatusBar
+            barStyle='dark-content'
+            hidden={false}
+            backgroundColor='#ffffff'
+            translucent={true}
+          />
+          <Layout style={styles.singpassLogo}>
+            <Image source={require('../img/SingPassLogo.jpg')} />
           </Layout>
-        </TouchableWithoutFeedback>
+          <Text style={loginStyle.header} category='h4'>
+            Log In
+          </Text>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Layout>
+              <Input
+                label='NRIC'
+                value={this.state.nric}
+                onChangeText={(text) => this.setState({nric: text})}
+              />
+              <Input
+                label='Password'
+                accessoryRight={this.renderIcon}
+                secureTextEntry={this.state.secureTextEntry}
+                value={this.state.password}
+                onChangeText={(text) => this.setState({password: text})}
+              />
+
+              <Button
+                style={styles.button}
+                status='danger'
+                onPress={() => this.handleVerify()}>
+                Login
+              </Button>
+              <Text style={loginStyle.message}>
+                {`This page is a simulation of SingPass MyInfo verification`}
+              </Text>
+              {responseMessage}
+            </Layout>
+          </TouchableWithoutFeedback>
+        </Layout>
       </Layout>
     );
   }
@@ -113,8 +141,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
-    marginLeft: 20,
-    marginRight: 20,
+    marginLeft: 40,
+    marginRight: 40,
+    marginBottom: 40,
+    justifyContent: 'center',
   },
   header: {
     marginTop: 20,
@@ -125,6 +155,9 @@ const styles = StyleSheet.create({
   button: {
     marginTop: 30,
   },
+  singpassLogo: {
+    alignItems: 'center',
+  },
 });
 
 const mapStateToProps = (state) => {
@@ -134,12 +167,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    editProfile: (user) => {
-      dispatch(editProfile(user));
-    },
-  };
-};
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(VerifyAccount);
